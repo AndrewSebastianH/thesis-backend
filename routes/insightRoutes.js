73c89d42906051ebd4088fb12fd068c5f502af8e
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2025-05-07 17:00:11 +0700

    fix: bug fix

--- routes/insightRoutes.js
+++ routes/insightRoutes.js
@@ -5,5 +5,7 @@ const { body } = require("express-validator");
 const insightController = require("../controller/insightController");
 const isAuthenticated = require("../middleware/isAuthenticated");
 
-router.get("/", isAuthenticated, insightController.getInsights);
+router.get("/", isAuthenticated, insightController.fetchUserInsight);
 // /insight?type=self or /insight?type=related
+
+module.exports = router;
